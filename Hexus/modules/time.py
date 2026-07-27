c45bd49d0e67c10b28a812a180bac84bf99cd488
wuffgame <kacper5555544444@gmail.com>
2026-07-27 22:43:13 +0200

    Added minute and second to time module

--- Hexus/modules/time.py
+++ Hexus/modules/time.py
@@ -3,6 +3,16 @@ class TimeHourNode:
     def __repr__(self):
         return f"TimeHourNode()"
 
+class TimeMinuteNode:
+    is_expression = True
+    def __repr__(self):
+        return f"TimeMinuteNode()"
+
+class TimeSecNode:
+    is_expression = True
+    def __repr__(self):
+        return f"TimeSecNode()"
+
 
 class TimeParser:
     name = "time"
@@ -13,12 +23,28 @@ class TimeParser:
         if token_type == "VAR" and value == "hour":
             parser.consume_value("VAR", "hour")
             return TimeParser.parse_hour(parser)
+        elif token_type == "VAR" and value == "minute":
+            parser.consume_value("VAR", "minute")
+            return TimeParser.parse_minute(parser)
+        elif token_type == "VAR" and value == "second":
+            parser.consume_value("VAR", "second")
+            return TimeParser.parse_second()
         raise SyntaxError(f"SyntaxError [time module]: Unknown command 'time.{value}'")
 
     @staticmethod
     def parse_hour(parser):
         parser.consume_end_of_statement()
         return TimeHourNode()
+
+    @staticmethod
+    def parse_minute(parser):
+        parser.consume_end_of_statement()
+        return TimeMinuteNode()
+
+    @staticmethod
+    def parse_second(parser):
+        parser.consume_end_of_statement()
+        return TimeSecNode()
 
 
 class TimeInterpreter:
@@ -30,4 +56,16 @@ class TimeInterpreter:
             hour = time.strftime("%H")
             return hour
 
+        def visit_TimeMinuteNode(self, node):
+            import time
+            minute = time.strftime("%M")
+            return minute
+
+        def visit_TimeSecNode(self, node):
+            import time
+            sec = time.strftime("%S")
+            return sec
+
         setattr(interpreter.__class__, "visit_TimeHourNode", visit_TimeHourNode)
+        setattr(interpreter.__class__, "visit_TimeMinuteNode", visit_TimeMinuteNode)
+        setattr(interpreter.__class__, "visit_TimeSecNode", visit_TimeSecNode)
